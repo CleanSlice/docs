b79c52b57b9bc1d08d81b3fa9e7888c9403b9bac
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2026-02-18 14:33:21 +0200

    feat: added google tag manager

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -8,6 +8,15 @@ export default defineConfig({
 
   head: [
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    [
+      'script',
+      {},
+      `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W7ZMZ49G');`,
+    ],
   ],
 
   themeConfig: {
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,10 +3,22 @@ import { h } from 'vue'
 import McpBanner from './McpBanner.vue'
 import './custom.css'
 
+const GtmNoscript = {
+  render() {
+    return h('noscript', h('iframe', {
+      src: 'https://www.googletagmanager.com/ns.html?id=GTM-W7ZMZ49G',
+      height: '0',
+      width: '0',
+      style: 'display:none;visibility:hidden',
+    }))
+  },
+}
+
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
+      'layout-top': () => h(GtmNoscript),
       'home-features-before': () => h(McpBanner),
     })
   },
